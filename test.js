f43fa9d3aace735881b571abaae76b22045f7dbd
Eric Mill <eric.mill@gsa.gov>
2014-10-13 03:33:45 -0400

    Respond to backpressure suggestions

--- test.js
+++ test.js
@@ -4,21 +4,21 @@ var concat = require('concat-stream');
 var frs = require('./');
 
 drop(document.body, function(files) {
-  var first = files[0], 
+  var first = files[0],
       s = frs(first),
       cursor, paused
 
   test('should read file when one is dropped', function(t) {
-    s.pipe(concat(function(contents) {        
+    s.pipe(concat(function(contents) {
         t.true(contents.length > 0)
-        t.end()       
+        t.end()
     }))
-  })  
-  
+  })
+
   s.on('progress', function(offset){
-  	if (offset / first.size < 0.5 || paused) return  	  		
+  	if (offset / first.size < 0.5 || paused) return
   	test('should pause when over 30% of the file is read', function(t) {
-	    cursor = s.pause()  				
+	    cursor = s.pause()
 	    t.true(cursor / first.size >= 0.5)
 	    t.end()
   	})
@@ -31,14 +31,14 @@ drop(document.body, function(files) {
   	  }, 2000)
   	})
 
-  	paused = true  	
+  	paused = true
   })
 
   s.on('end', function(offset){
   	test('should return correct offset upon end event', function(t) {
 	    t.true(first.size === offset)
 	    t.end()
-  	})		
+  	})
   })
 
 })
