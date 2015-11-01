680ae14989e1a4bcd6859e3ef52394c0808bbcda
Max Ogden <max@maxogden.com>
2015-11-01 09:45:30 -0800

    rewrite to use from2

--- index.js
+++ index.js
@@ -1,95 +1,34 @@
-var inherits = require('inherits')
-var EventEmitter = require('events').EventEmitter
+/* global FileReader */
+var from2 = require('from2')
+var toBuffer = require('typedarray-to-buffer')
 
-module.exports = FileStream
-
-function FileStream(file, options) { 
-  if (!(this instanceof FileStream))
-    return new FileStream(file, options)
+module.exports = function (file, options) {
   options = options || {}
-  options.output = options.output || 'arraybuffer'
-  this.options = options
-  this._file = file
-  this.readable = true
-  this.offset = options.offset || 0
-  this.paused = false
-  this.chunkSize = this.options.chunkSize || 8128  
+  var offset = options.offset || 0
+  var chunkSize = options.chunkSize || 8192
+  var fileReader = new FileReader(file)
 
-  var tags = ['name','size','type','lastModifiedDate']
-  tags.forEach(function (thing) {
-     this[thing] = file[thing]
-   }, this)      
-}
-
-  
-FileStream.prototype._FileReader = function() {
-  var self = this
-  var reader = new FileReader()
-  var outputType = this.options.output
-
-  reader.onloadend = function loaded(event) {
-    var data = event.target.result      
-    if (data instanceof ArrayBuffer)
-      data = new Buffer(new Uint8Array(event.target.result))
-    self.dest.write(data)        
-    if (self.offset < self._file.size) {
-      self.emit('progress', self.offset)
-      !self.paused && self.readChunk(outputType)      
-      return
+  var from = from2(function (size, cb) {
+    if (offset >= file.size) return cb(null, null)
+    fileReader.onloadend = function loaded (event) {
+      var data = event.target.result
+      if (data instanceof ArrayBuffer) data = toBuffer(new Uint8Array(event.target.result))
+      cb(null, data)
     }
-    self._end()
+    var end = offset + chunkSize
+    var slice = file.slice(offset, end)
+    fileReader.readAsArrayBuffer(slice)
+    offset = end
+  })
+
+  from.name = file.name
+  from.size = file.size
+  from.type = file.type
+  from.lastModifiedDate = file.lastModifiedDate
+
+  fileReader.onerror = function (err) {
+    from.destroy(err)
   }
-  reader.onerror = function(e) {
-    self.emit('error', e.target.error)
-  }
 
-  return reader
+  return from
 }
-
-FileStream.prototype.readChunk = function(outputType) {
-  var end = this.offset + this.chunkSize
-  var slice = this._file.slice(this.offset, end)
-  this.offset = end
-  if (outputType === 'binary')
-    this.reader.readAsBinaryString(slice)
-  else if (outputType === 'dataurl')
-    this.reader.readAsDataURL(slice)
-  else if (outputType === 'arraybuffer')
-    this.reader.readAsArrayBuffer(slice)
-  else if (outputType === 'text')
-    this.reader.readAsText(slice)  
-}
-
-FileStream.prototype._end = function() {
-  if (this.dest !== console && (!this.options || this.options.end !== false)) {
-    this.dest.end && this.dest.end()
-    this.dest.close && this.dest.close()
-    this.emit('end', this._file.size)
-  }  
-}
-
-FileStream.prototype.pipe = function pipe(dest, options) {
-  this.reader = this._FileReader()
-  this.readChunk(this.options.output)
-  this.dest = dest
-  return dest
-}
-
-FileStream.prototype.pause = function() {
-  this.paused = true
-  return this.offset
-}
-
-FileStream.prototype.resume = function() {
-  this.paused = false
-  this.readChunk(this.options.output)
-}
-
-FileStream.prototype.abort = function() {
-  this.paused = true
-  this.reader.abort()
-  this._end()
-  return this.offset
-}
-
-inherits(FileStream, EventEmitter)
--- test.js
+++ test.js
@@ -1,44 +1,25 @@
-var test = require('tape');
-var drop = require('drag-and-drop-files');
-var concat = require('concat-stream');
-var frs = require('./');
+var test = require('tape')
+var through = require('through2')
+var drop = require('drag-and-drop-files')
+var frs = require('./')
 
-drop(document.body, function(files) {
-  var first = files[0], 
-      s = frs(first),
-      cursor, paused
+drop(document.body, function (files) {
+  var first = files[0]
+  var LEN = 4096
+  var s = frs(first, {chunkSize: 4096})
 
-  test('should read file when one is dropped', function(t) {
-    s.pipe(concat(function(contents) {        
-        t.true(contents.length > 0)
-        t.end()       
-    }))
-  })  
-  
-  s.on('progress', function(offset){
-  	if (offset / first.size < 0.5 || paused) return  	  		
-  	test('should pause when over 30% of the file is read', function(t) {
-	    cursor = s.pause()  				
-	    t.true(cursor / first.size >= 0.5)
-	    t.end()
-  	})
-
-  	test('should resume 2 seconds after pause', function(t) {
-  	  setTimeout(function(){
-   	    s.resume()
-  	    t.true(s.paused === false)
-  	    t.end()
-  	  }, 2000)
-  	})
-
-  	paused = true  	
+  test('should read file when one is dropped', function (t) {
+    var buffs = []
+    var concatter = through(function (ch, enc, cb) {
+      buffs.push(ch)
+      t.ok(ch.length <= LEN, 'length is <= ' + LEN)
+      cb()
+    }, function () {
+      var all = Buffer.concat(buffs)
+      t.ok(all.length > 0, 'got some data')
+      t.equal(all.length, s.size, 'size is ' + s.size)
+      t.end()
+    })
+    s.pipe(concatter)
   })
-
-  s.on('end', function(offset){
-  	test('should return correct offset upon end event', function(t) {
-	    t.true(first.size === offset)
-	    t.end()
-  	})		
-  })
-
 })
